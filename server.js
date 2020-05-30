2ecf21843dcd791ed9fa9ba89ff135d70f6f337d
Jack <jackcurran2000@gamil.com>
2020-05-30 16:30:44 +1000

    added heroku config

---
+++ server.js
@@ -0,0 +1,17 @@
+//install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//serve only the static files from the dist
+app.use(express.static('.dist/AFL'));
+
+app.get('/*', function(req(res)){
+
+    res.sendFile)path.join(__dirname,'/dist/AFL}/index.html'));
+
+});
+
+//start the app by listening to the default heroku port
+app.listen(process.env.PORT || 8080);
