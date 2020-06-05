a3e38e8f12a6da5c174899f278ccb39322a25cd5
Jack <jackcurran2000@gamil.com>
2020-06-05 17:07:14 +1000

    final

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-//install express server
+//create a server.js file in the root folder of your project and if required
+// replace 'TestApp' with the name of your app paste in the below code 
+
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-//serve only the static files from the dist
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/AFL'));
 
-app.get('/*', function(req,res){
-
-    res.sendFile(path.join(__dirname,'/dist/AFL}/index.html'));
-
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/TestApp/index.html'));
 });
 
-//start the app by listening to the default heroku port
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
